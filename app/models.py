ccbfb74974ef8b9090f4b2a585b3652860032fb1
Muhammad Abdullah <muhammad.abdullah9@ontariotechu.net>
2026-03-04 16:34:28 -0500

    Made change to user account creation in database

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class User(UserMixin, db.Model):
     id         = db.Column(db.Integer, primary_key=True)
     username   = db.Column(db.String(80),  unique=True, nullable=False)
     password   = db.Column(db.String(200), nullable=False)
-
+    is_admin = db.Column(db.Boolean, default=False)
 
     def set_password(self, plain_user_password):
         self.password = generate_password_hash(plain_user_password)
