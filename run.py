2dc35823f103a91424602f003709627c8bd5c5a9
DonaldPmurt <hanfu.xiao@ontariotechu.net>
2026-03-03 23:53:18 -0500

    Basic server route was established, logout function entry created.

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
+from flask import render_template
+
 from app import create_app
 
 app = create_app()
+
+@app.route("/")
+def home():
+    return render_template("home.html")    
+
+@app.route("/create-admin")
+def create_admin():
+    return render_template("admin_account.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
