341f8d0dc647d2ad8c78513f516e67916edf9acd
DonaldPmurt <hanfu.xiao@ontariotechu.net>
2026-03-22 21:42:58 -0400

    add flask-migrate, no more manual db deletion

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
-from flask import Flask 
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
 db = SQLAlchemy()
 login_manager = LoginManager()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
@@ -10,6 +12,7 @@ def create_app():
 
     db.init_app(app)
     login_manager.init_app(app)
+    migrate.init_app(app, db)
     login_manager.login_view = 'auth.login'
 
     @login_manager.user_loader
