e1b1b20ca1a8d9779b263364095e2e71211db185
Sajan <sajan.selvsangar@ontariotechu.net>
2026-04-12 20:23:40 -0400

    Added comments to run.py

--- run.py
+++ run.py
@@ -2,7 +2,9 @@ from flask import render_template
 
 from app import create_app
 
+#Creates and load the application to be run
 app = create_app()
 
+#Run the application
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
